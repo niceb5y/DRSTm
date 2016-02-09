dbb03327b7e85d14ffd0c40ae9eb900ca45786a6
김승호 <niceb5y@gmail.com>
2016-02-09 10:19:00 +0900

    くっくっ

---
+++ DRSTKit/DRSTKit.swift
@@ -0,0 +1,13 @@
+//
+//  DRSTKit.swift
+//  DRST manager
+//
+//  Created by 김승호 on 2016. 2. 9..
+//  Copyright © 2016년 Seungho Kim. All rights reserved.
+//
+
+import UIKit
+
+public class DRSTKit: NSObject {
+	public static let Kisaragi_Chihaya = 72 //くっ
+}
